39696c2f0a9737bb1c11e44602179a6f85d1638a
k-diem <142491589+k-diem@users.noreply.github.com>
2024-03-05 16:49:43 +0700

    5_C# Basic - Slide 19

--- DemoMVC/Controllers/DemoController.cs
+++ DemoMVC/Controllers/DemoController.cs
@@ -1,11 +1,20 @@
-// DUONG KHANH DIEM - 2021050130
 using Microsoft.AspNetCore.Mvc;
+
 namespace DemoMVC.Controllers
 {
     public class DemoController : Controller
     {
+        // Khai báo hằng số kiểu string
+        private const string StudentName = " Duong Khanh Diem";
+
+        // Khai báo hằng số kiểu int
+        private readonly int StudentAge = 2021050130;
+
         public IActionResult Index()
         {
+            // Truyền giá trị của hằng số vào ViewBag để hiển thị trong view
+            ViewBag.StudentName = StudentName;
+            ViewBag.StudentAge = StudentAge;
             return View(); // Tạo một cái view tương ứng với action
         }
 
