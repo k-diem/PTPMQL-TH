1cc1db7e07ce035c1c55b00ebc7b6ae5059dc8c2
k-diem <142491589+k-diem@users.noreply.github.com>
2024-03-21 23:44:36 +0700

    CRUD du lieu

--- DemoMVC/Controllers/PersonController.cs
+++ DemoMVC/Controllers/PersonController.cs
@@ -1,21 +1,113 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DemoMVC.Data;
 using DemoMVC.Models;
 
 namespace DemoMVC.Controllers
 {
     public class PersonController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public PersonController(ApplicationDbContext context)
         {
-            return View(new Person()); // Tạo một đối tượng Person mới và truyền vào view
+            _context = context;
         }
-
-        [HttpPost]
-        public IActionResult Index(Person person)
+        public async Task<IActionResult> Index()
         {
-            ViewBag.thongBao = "Xin chao " + person.PersonId + " - " + person.FullName + " - " + person.Address ; // Hiển thị thông tin đối tượng Person
-            return View(person); // Trả về view với đối tượng Person đã nhập
+            var model = await _context.Person.ToListAsync();
+           return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonId, FullName, Address, Age")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+            var person = await _context.Person.FindAsync(id);
+            if (person == null) 
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("PersonId, FullName, Address, Age")] Person person)
+        {
+            if (id != person.PersonId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+            var person = await _context.Person.FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Person == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Person' is null");
+            }
+            var person = await _context.Person.FindAsync(id);
+            if (person != null)
+            {
+                _context.Person.Remove(person);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool PersonExists(string id)
+        {
+            return (_context.Person?.Any(e => e.PersonId == id)).GetValueOrDefault();
         }
     }
 }
-// DUONG KHANH DIEM - 2021050130
