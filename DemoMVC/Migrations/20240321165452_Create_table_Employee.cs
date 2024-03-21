a0ef61e058d1e7fe734a173a0440579908de1ad9
k-diem <142491589+k-diem@users.noreply.github.com>
2024-03-21 23:55:37 +0700

    7_MVC - Slide 67 - Employee

---
+++ DemoMVC/Migrations/20240321165452_Create_table_Employee.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_table_Employee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Persons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    PersonId = table.Column<string>(type: "TEXT", nullable: false),
+                    EmployeeId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.PersonId);
+                    table.ForeignKey(
+                        name: "FK_Employees_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "PersonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Persons");
+        }
+    }
+}
--- DemoMVC/Models/Employee.cs
+++ DemoMVC/Models/Employee.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+// DUONG KHANH DIEM - 2021050130
 namespace DemoMVC.Models
 {
+    [Table("Employees")]
     public class Employee : Person
     {
         public string EmployeeId { get; set; }
         public int Age { get; set; }
     }
 }
-
-// DUONG KHANH DIEM - 2021050130
